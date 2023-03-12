93b7f62c098fe0fdf791564084289f5c1914cd42
Valdemir Voltatoni Júnior <96141482+XDeathFoxX@users.noreply.github.com>
2023-03-12 18:20:13 -0300

    Update primeiro_programa.java

--- primeiro_programa.java
+++ primeiro_programa.java
@@ -3,7 +3,7 @@ package primeiro_programa;
 class app{
 
     public static void main(String[] args)  {
-        System.out.println("Olá mundo,esse é meu primeiro programa em Java 	😄");
+        System.out.println("Olá mundo,esse é meu primeiro programa em Java 😄");
     }
 
 }
